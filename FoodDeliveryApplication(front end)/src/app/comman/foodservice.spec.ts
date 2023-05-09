1848799eba484912121d87a0e09dad28366de628
ruturajbodare <ruturajbd22@gmail.com>
2023-05-09 22:47:40 +0530

    commit message

---
+++ FoodDeliveryApplication(front end)/src/app/comman/foodservice.spec.ts
@@ -0,0 +1,7 @@
+import { Foodservice } from './foodservice';
+
+describe('Foodservice', () => {
+  it('should create an instance', () => {
+    expect(new Foodservice()).toBeTruthy();
+  });
+});
---
+++ FoodDeliveryApplication(front end)/src/app/comman/foodservice.ts
@@ -0,0 +1,7 @@
+export class Foodservice {
+    constructor(public foodid:number,public foodname:string,public fooddis:string,public foodprice:number)
+    {
+
+    }
+
+}
